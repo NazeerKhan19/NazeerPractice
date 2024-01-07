a98ef47deaf3bbc9431cdbb3a8f8e30851a33e3e
nazeer <pattannazeer2001@gmail.com>
2024-01-07 12:47:47 +0530

    IF condition

--- practice.java
+++ practice.java
@@ -2,5 +2,21 @@ public class practice {
 
     public static void main(String[] args){
         System.out.println("Hello World..!");
+
+        int a = 10;
+        int b = 25;
+        System.out.println(a+b);
+
+        int x = 55;
+        int y = 55;
+        if (x>y){
+            System.out.println("Nazeer Khan");
+        }
+        else if (x==y){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("No");
+        }
     }
 }
