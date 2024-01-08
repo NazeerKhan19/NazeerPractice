2e0b12162f9a34e5641a3cde95965fd0d4110895
nazeer <pattannazeer2001@gmail.com>
2024-01-08 19:37:38 +0530

    Age Code

---
+++ practice1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+
+public class practice1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Age : ");
+        int Age = scan.nextInt();
+        scan.close();
+        
+        if(Age >= 18){
+            System.out.println("Applicable for Vote");
+        }
+        else {
+            System.out.println("Not applicable for Vote");
+        }
+    }
+}
